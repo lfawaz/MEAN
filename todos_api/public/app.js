47139660a9bf335e53d5f6c0f14c65f7404a953a
593296 <labibfawaz@C02V21FGHTDG.local>
2017-10-24 12:56:36 -0400

    add update functionality

--- todos_api/public/app.js
+++ todos_api/public/app.js
@@ -8,9 +8,16 @@ $(document).ready(function(){
     }
   })
 
+  $(".list").on('click', 'li', function(){
+    updateTodo($(this))
+  })
+
 	$(".list").on('click', 'span',
-	function(){
-		deleteTodo($(this).parent())
+	function(e){
+    e.stopPropagation();
+    console.log("span clicked!")
+		deleteTodo($(this).parent());
+
 	}
 )
 
@@ -23,8 +30,9 @@ function getTodos(todos){
 
 
 function addTodo(todo){
-  var newTodo = $('<li class="task">' + todo.name + '<span>X</span></li>')
-	newTodo.data('id', todo._id)
+  var newTodo = $('<li class="task">' + todo.name + '<span>X</span></li>');
+	newTodo.data('id', todo._id);
+  newTodo.data('completed', todo.completed);
   if(todo.completed){
     newTodo.addClass("done")
   }
@@ -40,8 +48,7 @@ function postTodo(){
 }
 
 function deleteTodo(todo){
-	taskId = todo.data('id');
-	deleteURL = '/api/todos/' + taskId;
+	deleteURL = '/api/todos/' + todo.data('id');
 	 $.ajax({
 		 method: 'DELETE',
 		 url: deleteURL,
@@ -53,3 +60,21 @@ function deleteTodo(todo){
 		 console.log(err)
 	 })
  }
+
+
+function updateTodo(todo){
+  var isDone = !todo.data('completed');
+  var updateURL = '/api/todos/' + todo.data('id');
+  $.ajax({
+    method: 'PUT',
+    url: updateURL,
+    data: {'completed': isDone}
+  }).then(function(data){
+    // console.log(data)
+    todo.toggleClass('done')
+    todo.data('completed', isDone)
+  }).catch(function(err){
+    console.log(err)
+  })
+
+}
